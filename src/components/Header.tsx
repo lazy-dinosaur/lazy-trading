ffc872dc863d43f79add28c3c3df138169d831ff
lazydino <woohs0130@naver.com>
2024-12-24 12:28:25 +0900

    Feat:Header component 구현중
    네비게이션 메뉴를 사이드바 형태로 구현할 예정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,14 @@
+import { useLocation } from "react-router";
+
 const Header = () => {
-  return <div></div>;
+  const { pathname } = useLocation();
+  const title = pathname.split("/")[1];
+
+  return (
+    <div className="flex justify-between items-center">
+      <h1 className="text-xl ">{title ? title : "Dashboard"}</h1>
+      <div>menu</div>
+    </div>
+  );
 };
 export default Header;
