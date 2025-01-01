f505140fd8e6430f4ead96fd6c427d9cdc335bc0
lazydino <woohs0130@naver.com>
2025-01-01 17:20:12 +0900

    Feat: ValidCheck 기능 추가

--- src/components/Locked.tsx
+++ src/components/Locked.tsx
@@ -100,85 +100,87 @@ const Locked = () => {
   };
 
   return (
-    <Card className="w-[400px]">
-      <CardHeader>
-        <CardTitle>Enter your pin number</CardTitle>
-        <CardDescription>
-          Its not validate your pin number this app will use the pin number to
-          unhash the hashed api keys
-        </CardDescription>
-      </CardHeader>
-      <CardContent>
-        {statusMessage.type && (
-          <div
-            className={`mb-4 p-3 rounded-md ${
-              statusMessage.type === "success"
-                ? "bg-green-100 text-green-800 border border-green-200"
-                : "bg-red-100 text-red-800 border border-red-200"
-            }`}
-          >
-            {statusMessage.message}
-          </div>
-        )}
-        <Form {...form}>
-          <form
-            onSubmit={form.handleSubmit(onSubmit)}
-            className="flex justify-between"
-          >
-            <FormField
-              control={form.control}
-              name="pin"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Enter PIN</FormLabel>
-                  <FormControl>
-                    <InputOTP
-                      maxLength={4}
-                      value={field.value}
-                      onChange={(value) => {
-                        const numericValue = value.replace(/\D/g, "");
-                        field.onChange(numericValue);
-                      }}
-                      className="space-x-10"
-                    >
-                      <InputOTPGroup>
-                        <InputOTPSlot index={0} />
-                      </InputOTPGroup>
-                      <InputOTPGroup>
-                        <InputOTPSlot index={1} />
-                      </InputOTPGroup>
-                      <InputOTPGroup>
-                        <InputOTPSlot index={2} />
-                      </InputOTPGroup>
-                      <InputOTPGroup>
-                        <InputOTPSlot index={3} />
-                      </InputOTPGroup>
-                    </InputOTP>
-                  </FormControl>
-                  <FormDescription>
-                    If you forget PIN, App will reset all data includes
-                    exchanges api keys
-                  </FormDescription>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <div className="flex flex-col gap-2 mt-6">
-              <Button type="submit" disabled={isValidating}>
-                {isValidating ? (
-                  <>
-                    <Loader2 className="mr-2 h-4 w-4 animate-spin" />
-                    Verifying...
-                  </>
-                ) : (
-                  "Submit"
-                )}
-              </Button>
+    <div className="w-full flex justify-center items-center">
+      <Card className="w-[400px]">
+        <CardHeader>
+          <CardTitle>Enter your pin number</CardTitle>
+          <CardDescription>
+            Its not validate your pin number this app will use the pin number to
+            unhash the hashed api keys
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          {statusMessage.type && (
+            <div
+              className={`mb-4 p-3 rounded-md ${
+                statusMessage.type === "success"
+                  ? "bg-green-100 text-green-800 border border-green-200"
+                  : "bg-red-100 text-red-800 border border-red-200"
+              }`}
+            >
+              {statusMessage.message}
             </div>
-          </form>
-        </Form>
-      </CardContent>
-    </Card>
+          )}
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="flex justify-between"
+            >
+              <FormField
+                control={form.control}
+                name="pin"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Enter PIN</FormLabel>
+                    <FormControl>
+                      <InputOTP
+                        maxLength={4}
+                        value={field.value}
+                        onChange={(value) => {
+                          const numericValue = value.replace(/\D/g, "");
+                          field.onChange(numericValue);
+                        }}
+                        className="space-x-10"
+                      >
+                        <InputOTPGroup>
+                          <InputOTPSlot index={0} />
+                        </InputOTPGroup>
+                        <InputOTPGroup>
+                          <InputOTPSlot index={1} />
+                        </InputOTPGroup>
+                        <InputOTPGroup>
+                          <InputOTPSlot index={2} />
+                        </InputOTPGroup>
+                        <InputOTPGroup>
+                          <InputOTPSlot index={3} />
+                        </InputOTPGroup>
+                      </InputOTP>
+                    </FormControl>
+                    <FormDescription>
+                      If you forget PIN, App will reset all data includes
+                      exchanges api keys
+                    </FormDescription>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="flex flex-col gap-2 mt-6">
+                <Button type="submit" disabled={isValidating}>
+                  {isValidating ? (
+                    <>
+                      <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+                      Verifying...
+                    </>
+                  ) : (
+                    "Submit"
+                  )}
+                </Button>
+              </div>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 export default Locked;
