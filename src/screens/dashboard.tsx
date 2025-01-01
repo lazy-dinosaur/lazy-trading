ff4d4c90d91b13d632e872e80321c7068376f12b
lazydino <woohs0130@naver.com>
2025-01-01 17:22:33 +0900

    Feat:초기 로딩 추가

--- src/screens/dashboard.tsx
+++ src/screens/dashboard.tsx
@@ -1,19 +1,33 @@
+import { LoadingSpinner } from "@/components/Loading";
 import { Button } from "@/components/ui/button";
 import { useAccounts } from "@/hooks/useAccounts";
+import { useAppStateCache } from "@/hooks/useAppStateCache";
+// import { useAppStateCache } from "@/hooks/useAppStateCache";
+// import { useEffect } from "react";
 import { useNavigate } from "react-router";
 
 const Dashboard = () => {
   const navigate = useNavigate();
   const { deleteAllAccounts } = useAccounts();
-  return (
-    <>
+  const { isLoaded } = useAppStateCache();
+  return isLoaded ? (
+    <div>
       Dashboard.tsx
       <Button onClick={() => navigate("/accounts")}>accounts</Button>
       <Button onClick={() => navigate("/trade")}>trade</Button>
       <Button onClick={() => navigate("/setup")}>setup</Button>
       <Button onClick={() => navigate("/search")}>search</Button>
-      <Button onClick={() => deleteAllAccounts()}>deleteAllAccounts</Button>
-    </>
+      <Button
+        onClick={() => {
+          deleteAllAccounts();
+          navigate("/setup");
+        }}
+      >
+        deleteAllAccounts
+      </Button>
+    </div>
+  ) : (
+    <LoadingSpinner />
   );
 };
 export default Dashboard;
