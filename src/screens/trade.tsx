ad7c51001b074a2320b3153a48ff63d8b78a7fd6
lazydino <woohs0130@naver.com>
2025-01-01 19:11:24 +0900

    Feat: 매매 화면에 로딩 컴포넌트 추가

--- src/screens/trade.tsx
+++ src/screens/trade.tsx
@@ -1,14 +1,30 @@
+import { LoadingSpinner } from "@/components/Loading";
 import { Button } from "@/components/ui/button";
+import useExchange from "@/hooks/useExchange";
+import { useEffect } from "react";
 import { useNavigate } from "react-router";
 const Trade = () => {
   const navigate = useNavigate();
+  const { fetchTicker } = useExchange();
+  const { data, isLoading } = fetchTicker;
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
-    <div>
-      Trade.tsx
-      <Button onClick={() => navigate("/accounts")}>accounts</Button>
-      <Button onClick={() => navigate("/trade")}>trade</Button>
-      <Button onClick={() => navigate("/setup")}>setup</Button>
-      <Button onClick={() => navigate("/search")}>search</Button>
+    <div className="w-[450px] h-full">
+      {isLoading ? (
+        <LoadingSpinner />
+      ) : (
+        <>
+          Trade.tsx
+          <Button onClick={() => navigate("/accounts")}>accounts</Button>
+          <Button onClick={() => navigate("/trade")}>trade</Button>
+          <Button onClick={() => navigate("/setup")}>setup</Button>
+          <Button onClick={() => navigate("/search")}>search</Button>
+        </>
+      )}
     </div>
   );
 };
