bdb2b010a77a1820082c8d03ec510047314915af
lazydino <woohs0130@naver.com>
2025-01-02 19:21:39 +0900

    Feat: 시간봉 선택 버튼과 계정 선택 메뉴 추가

--- src/screens/trade.tsx
+++ src/screens/trade.tsx
@@ -1,30 +1,32 @@
 import { LoadingSpinner } from "@/components/Loading";
-import { DecryptedAccount, useAccounts } from "@/hooks/useAccounts";
-import useExchange from "@/hooks/useExchange";
-import { useEffect, useState } from "react";
-import { useParams } from "react-router";
-import { PriceInfo } from "./Trade/price-info";
+import { AccountSelector } from "@/components/trade/account-select";
+import { Chart } from "@/components/trade/chart";
+import { PriceInfo } from "@/components/trade/price-info";
+import { TimeFrameType, TimeFrame } from "@/components/trade/time-frame";
+import { DecryptedAccount } from "@/hooks/useAccounts";
+import { useTicker } from "@/hooks/useExchange";
+import { useState } from "react";
 
 const Trade = () => {
-  const { fetchTicker } = useExchange();
-  const { data: tickerData, isLoading } = fetchTicker;
-  const { useAllDecryptedAccounts } = useAccounts();
-  const { exchange } = useParams();
-  const { data } = useAllDecryptedAccounts();
+  const { data: tickerData, isLoading } = useTicker();
   const [accounts, setAccounts] = useState<DecryptedAccount[]>();
-
-  useEffect(() => {
-    if (!accounts && data)
-      setAccounts(() =>
-        Object.values(data).filter((value) => value.exchange === exchange),
-      );
-  }, [data]);
+  const [selected, setSelected] = useState<number>(0);
+  const [timeFrame, setTimeFrame] = useState<TimeFrameType>("30");
 
   return (
-    <div className="w-[450px] h-full">
+    <div className="w-[450px] h-full space-y-3">
       {!isLoading && tickerData ? (
         <>
           <PriceInfo data={tickerData} />
+          <div className="w-full flex items-center justify-between">
+            <TimeFrame timeFrameState={{ timeFrame, setTimeFrame }} />
+            <AccountSelector
+              accountState={{ accounts, setAccounts }}
+              selectedState={{ selected, setSelected }}
+              exchange={tickerData.exchange}
+            />
+          </div>
+          <Chart timeFrame={timeFrame} tickerData={tickerData} />
         </>
       ) : (
         <LoadingSpinner />
