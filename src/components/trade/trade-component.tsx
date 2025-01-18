874e886c46bc2db7a194a9bab315569cd97e150f
lazydino <woohs0130@naver.com>
2025-01-18 16:38:17 +0900

    Refactor: 리엑트쿼리 훅으로 변경하면서 불필요한 코드 삭제

--- src/components/trade/trade-component.tsx
+++ src/components/trade/trade-component.tsx
@@ -1,12 +1,12 @@
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+// import { Dispatch, SetStateAction, useEffect, useState } from "react";
 import { Button } from "../ui/button";
-import { getTradingConfig, setTradingConfig } from "@/lib/appStorage";
+// import { getTradingConfig, setTradingConfig } from "@/lib/appStorage";
 import { ChevronUp, ChevronDown } from "lucide-react";
+import { AccountInfo, AccountInfoType } from "@/hooks/useAccountsInfo";
 import {
-  AccountInfo,
-  AccountInfoType,
-  BalancesType,
-} from "@/hooks/useAccountsInfo";
+  useFetchTradingConfig,
+  useMutateTradingConfig,
+} from "@/hooks/settings";
 import { useParams } from "react-router";
 
 export const TradeComponent = ({
@@ -16,129 +16,104 @@ export const TradeComponent = ({
   id?: string;
   accountsInfo?: AccountInfoType;
 }) => {
-  const [risk, setRisk] = useState<number | null>(null);
-
-  // 초기 설정 로드
-  useEffect(() => {
-    const loadInitialConfig = async () => {
-      try {
-        const tradingConfig = await getTradingConfig();
-        if (tradingConfig?.risk) {
-          setRisk(tradingConfig.risk);
-        } else {
-          setRisk(1.5); // 기본값
-        }
-      } catch (error) {
-        console.error("Failed to load trading config:", error);
-        setRisk(1.5); // 에러 시 기본값
-      }
-    };
-
-    loadInitialConfig();
-  }, []); // 컴포넌트 마운트 시 한 번만 실행
-
-  // risk 값이 변경될 때 저장
-  useEffect(() => {
-    const saveConfig = async () => {
-      if (risk !== null) {
-        try {
-          await setTradingConfig({ risk });
-          console.log("Trading config saved:", { risk });
-        } catch (error) {
-          console.error("Failed to save trading config:", error);
-        }
-      }
-    };
-    saveConfig();
-  }, [risk]); // risk가 변경될 때만 실행
-  useEffect(() => {
-    console.log(accountsInfo);
-  }, [accountsInfo]);
-
   return (
-    <div className="w-full h-40">
-      <div className="h-3/5 w-full grid grid-cols-12 pb-2 gap-1">
-        <AssetInfo
-          assets={accountsInfo && !!id ? accountsInfo[id].balance : undefined}
-        />
+    <div className="w-full h-full flex-1 flex flex-col space-y-3">
+      <div className="flex w-full h-2/5 items-center justify-between">
         <TradingInfo
           accountsInfo={accountsInfo && !!id ? accountsInfo[id] : undefined}
         />
-        <RiskSetting riskState={{ risk, setRisk }} />
+        <TradingAction />
       </div>
-      <div className="w-full flex justify-between h-2/5 items-center">
-        <Button variant={"long"} className="w-1/2 mx-2 opacity-90">
+      <div className="h-full w-full flex">ddd</div>
+    </div>
+  );
+};
+
+const TradingAction = () => {
+  return (
+    <div className="flex w-1/3 min-w-32 max-w-64 flex-col items-center justify-between h-full">
+      <RiskSetting />
+      <TradingSetting />
+      <div className="flex flex-col w-full gap-2">
+        <Button variant={"long"} className="opacity-90">
           LONG
         </Button>
-        <Button variant={"short"} className="w-1/2 mx-2 opacity-90">
+        <Button variant={"short"} className="opacity-90">
           SHORT
         </Button>
       </div>
     </div>
   );
 };
-const AssetInfo = ({ assets }: { assets: BalancesType | undefined }) => {
+// const AssetInfo = ({ assets }: { assets: BalancesType | undefined }) => {
+//   const { base } = useParams();
+//   const baseAsset = base?.split(":")[1];
+//   const baseAssetInfo = baseAsset && assets && assets?.[baseAsset];
+//   const total = assets?.usd.total;
+//   const used = assets?.usd.used;
+//   const free = assets?.usd.free;
+//
+//   return (
+//     <>
+//       <div className="h-full col-span-1">
+//         <div className="text-sm text-muted-foreground mb-1">Assets</div>
+//         <div className="capitalize">total:</div>
+//         <div className="capitalize">free:</div>
+//         <div className="capitalize">used:</div>
+//       </div>
+//       <div className="h-full col-span-2">
+//         <div className="text-xs text-muted-foreground mb-1">InUSD</div>
+//         {assets ? (
+//           <>
+//             <div className="capitalize">${total}</div>
+//             <div className="capitalize">${free}</div>
+//             <div className="capitalize">${used}</div>
+//           </>
+//         ) : (
+//           <>
+//             <div className="">스켈레톤 로딩 예정</div>
+//           </>
+//         )}
+//       </div>
+//       <div className="h-full col-span-2">
+//         {assets ? (
+//           <>
+//             <div className="text-xs text-muted-foreground mb-1">
+//               {baseAsset}
+//             </div>
+//             <div className="capitalize">
+//               {baseAssetInfo ? baseAssetInfo.total : 0}
+//             </div>
+//             <div className="capitalize">
+//               {baseAssetInfo ? baseAssetInfo.free : 0}
+//             </div>
+//             <div className="capitalize">
+//               {baseAssetInfo ? baseAssetInfo.free : 0}
+//             </div>
+//           </>
+//         ) : (
+//           <>
+//             <div className="">스켈레톤 로딩 예정</div>
+//           </>
+//         )}
+//       </div>
+//     </>
+//   );
+// };
+
+const TradingInfo = ({ accountsInfo }: { accountsInfo?: AccountInfo }) => {
+  const assets = accountsInfo?.balance;
   const { base } = useParams();
   const baseAsset = base?.split(":")[1];
   const baseAssetInfo = baseAsset && assets && assets?.[baseAsset];
   const total = assets?.usd.total;
   const used = assets?.usd.used;
   const free = assets?.usd.free;
-
   return (
-    <>
-      <div className="h-full col-span-1">
-        <div className="text-xs text-muted-foreground mb-1">Assets</div>
-        <div className="capitalize">total:</div>
-        <div className="capitalize">free:</div>
-        <div className="capitalize">used:</div>
-      </div>
-      <div className="h-full col-span-2">
-        <div className="text-xs text-muted-foreground mb-1">InUSD</div>
-        {assets ? (
-          <>
-            <div className="capitalize">${total}</div>
-            <div className="capitalize">${free}</div>
-            <div className="capitalize">${used}</div>
-          </>
-        ) : (
-          <>
-            <div className="">스켈레톤 로딩 예정</div>
-          </>
-        )}
-      </div>
-      <div className="h-full col-span-2">
-        {assets ? (
-          <>
-            <div className="text-xs text-muted-foreground mb-1">
-              {baseAsset}
-            </div>
-            <div className="capitalize">
-              {baseAssetInfo ? baseAssetInfo.total : 0}
-            </div>
-            <div className="capitalize">
-              {baseAssetInfo ? baseAssetInfo.free : 0}
-            </div>
-            <div className="capitalize">
-              {baseAssetInfo ? baseAssetInfo.free : 0}
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="">스켈레톤 로딩 예정</div>
-          </>
-        )}
-      </div>
-    </>
-  );
-};
-
-const TradingInfo = ({ accountsInfo }: { accountsInfo?: AccountInfo }) => {
-  return (
-    <div className="h-full border col-span-5">
-      <div className="text-xs text-muted-foreground mb-1">TradingInfo</div>
+    <div className="h-full w-full flex flex-col items-center justify-between">
       {accountsInfo ? (
         <>
+          <div className="text-xs text-muted-foreground mb-1">TradingInfo</div>
           <div className="capitalize text-muted-foreground">StopLossHigh</div>
           <div className="capitalize text-muted-foreground">StopLossLow</div>
         </>
@@ -147,54 +122,140 @@ const TradingInfo = ({ accountsInfo }: { accountsInfo?: AccountInfo }) => {
           <div className="">스켈레톤 로딩 예정</div>
         </>
       )}
+      <span className="grid grid-cols-7 grid-rows-3 w-full">
+        <span className="text-xs text-muted-foreground capitalize">
+          balance
+        </span>
+        <span className="text-xs text-muted-foreground capitalize col-span-2">
+          total
+        </span>
+        <span className="text-xs text-muted-foreground capitalize col-span-2">
+          free
+        </span>
+        <span className="text-xs text-muted-foreground capitalize col-span-2">
+          used
+        </span>
+        <span className="text-xs text-muted-foreground capitalize ">
+          {baseAsset}
+        </span>
+        <span className="text-sm capitalize col-span-2">
+          {baseAssetInfo ? baseAssetInfo.total : 0}
+        </span>
+        <span className="text-sm capitalize col-span-2">
+          {baseAssetInfo ? baseAssetInfo.free : 0}
+        </span>
+        <span className="text-sm capitalize col-span-2">
+          {baseAssetInfo ? baseAssetInfo.used : 0}
+        </span>
+        <span className="text-xs text-muted-foreground capitalize">Usd</span>
+        <span className="text-sm capitalize col-span-2">
+          {total ? total : 0}
+        </span>
+        <span className="text-sm capitalize col-span-2">
+          {used ? total : 0}
+        </span>
+        <span className="text-sm capitalize col-span-2">
+          {free ? total : 0}
+        </span>
+      </span>
     </div>
   );
 };
 
-const RiskSetting = ({
-  riskState,
-}: {
-  riskState: {
-    risk: number | null;
-    setRisk: Dispatch<SetStateAction<number | null>>;
-  };
-}) => {
-  const { risk, setRisk } = riskState;
+const TradingSetting = () => {
+  const { data: tradingConfig } = useFetchTradingConfig();
+  const { mutate: setTradingConfig, isPending } = useMutateTradingConfig();
 
   return (
-    <div className="h-full col-span-2">
-      <div className="flex h-full w-full items-center justify-center">
-        <span className="flex h-full w-full items-center justify-center flex-col">
-          <span className="text-xs text-muted-foreground">Risk</span>
-          <span className="text-sm font-semibold">{risk}%</span>
+    <div className="flex w-full items-center justify-center">
+      <span className="flex h-full w-full items-center justify-center flex-col">
+        <span className="text-xs text-muted-foreground">R/R</span>
+        <span className="text-sm font-semibold">
+          {tradingConfig?.riskRatio} : 1
         </span>
-        <span className="flex h-full w-full items-center justify-center gap-1 flex-col text-center">
-          <Button
-            variant="ghost"
-            size="icon"
-            className="h-6 w-6 hover:bg-accent border-t"
-            onClick={() =>
-              setRisk((value) =>
-                value ? (value + 0.5 > 5 ? 5 : value + 0.5) : 1.5,
-              )
-            }
-          >
-            <ChevronUp className="h-4 w-4" />
-          </Button>
-          <Button
-            variant="ghost"
-            size="icon"
-            className="h-6 w-6 hover:bg-accent border-b"
-            onClick={() =>
-              setRisk((value) =>
-                value ? (value - 0.5 < 0.5 ? 0.5 : value - 0.5) : 1.5,
-              )
-            }
-          >
-            <ChevronDown className="h-4 w-4" />
-          </Button>
-        </span>
-      </div>
+      </span>
+      <span className="flex w-full items-center justify-center gap-1 flex-col text-center">
+        <Button
+          variant="ghost"
+          size="icon"
+          className="h-6 w-6 hover:bg-accent border-t"
+          disabled={isPending}
+          onClick={() => {
+            const riskRatio = tradingConfig?.riskRatio
+              ? tradingConfig.riskRatio + 0.5 > 5
+                ? 5
+                : tradingConfig.riskRatio + 0.5
+              : 1.5;
+            setTradingConfig({ riskRatio });
+          }}
+        >
+          <ChevronUp className="h-4 w-4" />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="h-6 w-6 hover:bg-accent border-b"
+          disabled={isPending}
+          onClick={() => {
+            const riskRatio = tradingConfig?.riskRatio
+              ? tradingConfig.riskRatio - 0.5 < 0.5
+                ? 0.5
+                : tradingConfig.riskRatio - 0.5
+              : 1.5;
+            setTradingConfig({ riskRatio });
+          }}
+        >
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </span>
+    </div>
+  );
+};
+
+const RiskSetting = () => {
+  const { data: tradingConfig } = useFetchTradingConfig();
+  const { mutate: setTradingConfig, isPending } = useMutateTradingConfig();
+
+  return (
+    <div className="flex w-full items-center justify-center">
+      <span className="flex h-full w-full items-center justify-center flex-col">
+        <span className="text-xs text-muted-foreground">Risk</span>
+        <span className="text-sm font-semibold">{tradingConfig?.risk}%</span>
+      </span>
+      <span className="flex w-full items-center justify-center gap-1 flex-col text-center">
+        <Button
+          variant="ghost"
+          size="icon"
+          className="h-6 w-6 hover:bg-accent border-t"
+          disabled={isPending}
+          onClick={() => {
+            const risk = tradingConfig?.risk
+              ? tradingConfig.risk + 0.5 > 5
+                ? 5
+                : tradingConfig.risk + 0.5
+              : 1.5;
+            setTradingConfig({ risk });
+          }}
+        >
+          <ChevronUp className="h-4 w-4" />
+        </Button>
+        <Button
+          variant="ghost"
+          size="icon"
+          className="h-6 w-6 hover:bg-accent border-b"
+          disabled={isPending}
+          onClick={() => {
+            const risk = tradingConfig?.risk
+              ? tradingConfig.risk - 0.5 < 0.5
+                ? 0.5
+                : tradingConfig.risk - 0.5
+              : 1.5;
+            setTradingConfig({ risk });
+          }}
+        >
+          <ChevronDown className="h-4 w-4" />
+        </Button>
+      </span>
     </div>
   );
 };
