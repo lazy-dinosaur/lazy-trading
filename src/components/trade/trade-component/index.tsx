4555690edf8860276538f15ae1c79c3dff23e97f
lazydino <woohs0130@naver.com>
2025-02-12 10:47:02 +0900

    Style: trade component 높이 최적화

--- src/components/trade/trade-component/index.tsx
+++ src/components/trade/trade-component/index.tsx
@@ -3,7 +3,7 @@ import { TradeInfo } from "./info";
 
 export const TradeComponent = () => {
   return (
-    <div className="flex w-full min-h-max items-center justify-between gap-3">
+    <div className="flex w-full min-h-min items-center justify-between gap-3">
       <TradeInfo />
       <TradingAction />
     </div>
