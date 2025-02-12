4fc9798af6383955eba7322dbcf4795c1b600688
lazydino <woohs0130@naver.com>
2025-02-12 11:27:07 +0900

    Feat: 미디어 쿼리 적용중

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -19,7 +19,10 @@ export default function Layout() {
 
   return (
     <SidebarProvider defaultOpen={false}>
-      <div className="h-[100vh] w-[100vw] flex flex-col space-y-6 p-6">
+      <div
+        id="screenWrapper"
+        className="h-[100vh] w-[100vw] flex flex-col space-y-3 p-3 xl:space-y-6 xl:p-6"
+      >
         <Outlet />
       </div>
       <AppSidebar />
